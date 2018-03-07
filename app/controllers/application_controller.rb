aabd8fbbebed4c473d702d971ba2a29f99b1a54e
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2018-03-07 13:16:25 +0100

    Finish Rails CMS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_filter :getPageNav
+  before_filter :getPageNav, :getJumbotron, :getBlocks
   protect_from_forgery with: :exception
 
+
+  def getJumbotron
+    @jumbotrons=Block.where("position='jumbotron'")
+  end
+
+  def getBlocks
+    @blocks=Block.where("position='block'").order(order: :asc)
+  end
 
   def getPageNav
     @pagesNav = Page.where("is_published = true && menu_display = true").order(order: :asc)
